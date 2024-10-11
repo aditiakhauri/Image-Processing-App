3dee6e53dc0cec5bbedd550913d27d1db8028796
aditiakhauri <73890308+aditiakhauri@users.noreply.github.com>
2024-10-11 14:38:23 +0530

    added public url for output image

--- app.py
+++ app.py
@@ -243,4 +243,4 @@ def serve_image(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    app.run(debug=True, host='0.0.0.0', port=8000)
